8a5f2fabb7c657b7e7a4a810097cdc1cb4f67755
kbarwinski <kajetan.barwinski@outlook.com>
2023-09-15 19:48:16 +0200

    prod

--- src/app/core/services/constants/baseurl.ts
+++ src/app/core/services/constants/baseurl.ts
@@ -1,4 +1,4 @@
-import { environment } from '../../../../environments/environment'
+import { environment } from '../../../../environments/environment.prod'
 
 const apiBaseUrl = environment.apiBaseUrl;
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://ocr-api-32d5db59ab69.herokuapp.com/'
+    apiBaseUrl: 'https://ocr-api-32d5db59ab69.herokuapp.com'
   };
