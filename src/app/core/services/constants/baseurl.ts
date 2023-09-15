c987fdd731632d655cbc64fb34655e1ae54712ce
kbarwinski <kajetan.barwinski@outlook.com>
2023-09-15 19:18:43 +0200

    api url

--- src/app/core/services/constants/baseurl.ts
+++ src/app/core/services/constants/baseurl.ts
@@ -1,5 +1,6 @@
-const winUrl = 'http://192.168.0.174:5245';
-const dockUrl = 'http://127.0.0.1:5245';
+import { environment } from '../../../../environments/environment'
 
-export const baseUrl = dockUrl;
+const apiBaseUrl = environment.apiBaseUrl;
+
+export const baseUrl = apiBaseUrl;
 
